3f8e515d7aa16979968519b4f36406955450ef0c
Dagit Pam <emmanuelpam49@gmail.com>
2020-02-21 03:49:20 +0100

    Fetching contacts from phone

--- app/src/main/java/com/codeplateau/whatsappclone/ContactActivity.kt
+++ app/src/main/java/com/codeplateau/whatsappclone/ContactActivity.kt
@@ -2,11 +2,40 @@ package com.codeplateau.whatsappclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.ListView
+import com.codeplateau.whatsappclone.Adapter.CustomAdapter
+import com.codeplateau.whatsappclone.model.ContactModel
 
 class ContactActivity : AppCompatActivity() {
+    private var listView: ListView? = null
+    private var customAdapter: CustomAdapter? = null
+    private var contactModelArrayList: ArrayList<ContactModel>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+        listView = findViewById(R.id.listView) as ListView
+
+        contactModelArrayList = ArrayList()
+
+        val phones = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC")
+        while (phones!!.moveToNext()) {
+            val name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+            val phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+
+            val contactModel = ContactModel()
+            contactModel.setNames(name)
+            contactModel.setNumbers(phoneNumber)
+            contactModelArrayList!!.add(contactModel)
+            Log.d("name>>", name + "  " + phoneNumber)
+        }
+        phones.close()
+
+        customAdapter = CustomAdapter(this, contactModelArrayList!!)
+        listView!!.adapter = customAdapter
+
     }
 }
---
+++ app/src/main/java/com/codeplateau/whatsappclone/model/ContactModel.kt
@@ -0,0 +1,22 @@
+package com.codeplateau.whatsappclone.model
+
+class ContactModel {
+    var name: String? = null
+    var number: String? = null
+
+    fun setNames(name: String) {
+        this.name = name
+    }
+
+    fun getNumbers(): String {
+        return number.toString()
+    }
+
+    fun setNumbers(number: String) {
+        this.number = number
+    }
+
+    fun getNames(): String {
+        return name.toString()
+    }
+}
